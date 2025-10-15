6444c9d2596550249e75317dd907bb7f6b5bbcfe
Tobii <ptrayoola@gmail.com>
2025-10-15 16:15:32 +0100

    Cargo Rust Practices

---
+++ week-3/practice_4/target/debug/practice_4.d
@@ -0,0 +1,1 @@
+C:\Users\Administrator\p.ayoolaCSC101\week-3\practice_4\target\debug\practice_4.exe: C:\Users\Administrator\p.ayoolaCSC101\week-3\practice_4\src\main.rs
---
+++ week-3/practice_8/target/debug/practice_8.d
@@ -0,0 +1,1 @@
+C:\Users\Administrator\p.ayoolaCSC101\week-3\practice_8\target\debug\practice_8.exe: C:\Users\Administrator\p.ayoolaCSC101\week-3\practice_8\src\main.rs
