350002390ac021199a8de41bd31594b2ce376c63
Tobii <ptrayoola@gmail.com>
2025-10-26 22:03:24 +0100

    Week 4 Latest

--- week-4/project_1/src/main.rs
+++ week-4/project_1/src/main.rs
@@ -5,7 +5,7 @@ fn main() {
 	let mut input2 = String::new();
 	let mut input3 = String::new();
 
-	println!("Program to find the nature of the Roots of a Quadratic Equation");
+	println!("Program to find the Roots of a Quadratic Equation");
 
 	println!("Enter a");
 	io::stdin().read_line(&mut input1).expect("Not a valid string");
@@ -19,20 +19,31 @@ fn main() {
 	io::stdin().read_line(&mut input3).expect("Not a valid string");
 	let c:i32 = input3.trim().parse().expect("Not a valid number");
 
-	let Determinant:i32  = (b * b) - (4 * a *c);
-	println!("The Determinant of the Quadratic Equation is {}", Determinant);
+	let determinant:i32  = (b * b) - (4 * a *c);
+	println!("The Determinant of the Quadratic Equation is {}", determinant);
 
-	if Determinant > 0 {
+	if determinant > 0 {
 		println!("There are two Distinct Roots.");
 	}
 
-	else if Determinant == 0 {
+	else if determinant == 0 {
 		println!("There is exactly one Real Root.");
 	}
 
-	else if Determinant < 0 {
+	else if determinant < 0 {
 		println!("There are no Real Roots.");
 	}
+    if determinant >= 0 {
 
+    let sqrt_d = determinant.isqrt(); 
+        let root1 = (-b + sqrt_d) / (2 * a);
+        let root2 = (-b - sqrt_d) / (2 * a);
+
+        println!("Root 1 = {}", root1);
+        println!("Root 2 = {}", root2);
+    }
+     else {
+    println!("There are no real roots (complex roots).")
+}
 
 }
